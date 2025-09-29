90167ecdaee8a194e7a0e2a6360bea51986ca58d
Almadhani <almadhani17@students.amikom.ac.id>
2025-09-29 15:53:56 +0700

    fix(db-connection): remove unnecessary connection and added port in server.js

--- server.js
+++ server.js
@@ -2,13 +2,13 @@
 require('dotenv').config({ path: '.env' });
 
 const app = require('./app');
-const sequelize = require('./database/connection');
-
-const PORT = process.env.PORT || 3001;
+const { sequelize } = require('./src/models');
+const PORT = process.env.PORT || 5015;
 
 const startServer = async () => {
   try {
     await sequelize.authenticate();
+
     app.listen(PORT, () => {
       console.log(`Server is running on port ${PORT}`);
     });
